4d5113c08d9c984e8f73d152e91e0ea8f78b8c30
KullerCode <kullerblue@icloud.com>
2026-07-17 21:41:54 -0400

    less Js

--- script.js
+++ script.js
@@ -44,62 +44,62 @@ setInterval(changeText, 3000);
  */
 // circle skill ///////////////////////////////////////////////
 
-const circles = document.querySelectorAll(".circle");
-circles.forEach((elem) => {
-  var dots = elem.getAttribute("data-dots");
-  var marked = elem.getAttribute("data-percent");
-  var percent = Math.floor((dots * marked) / 100);
-  var points = "";
-  var rotate = 360 / dots;
+// const circles = document.querySelectorAll(".circle");
+// circles.forEach((elem) => {
+//   var dots = elem.getAttribute("data-dots");
+//   var marked = elem.getAttribute("data-percent");
+//   var percent = Math.floor((dots * marked) / 100);
+//   var points = "";
+//   var rotate = 360 / dots;
 
-  for (let i = 0; i < dots; i++) {
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-  }
+//   for (let i = 0; i < dots; i++) {
+//     points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+//   }
 
-  elem.innerHTML = points;
+//   elem.innerHTML = points;
 
-  const pointsMarked = elem.querySelectorAll(".points");
-  for (let i = 0; i < percent; i++) {
-    pointsMarked[i].classList.add("marked");
-  }
-});
+//   const pointsMarked = elem.querySelectorAll(".points");
+//   for (let i = 0; i < percent; i++) {
+//     pointsMarked[i].classList.add("marked");
+//   }
+// });
 
 // Active menu  ///////////////////////////////////////////////
 
-let menuLi = document.querySelectorAll("header ul li a");
-let section = document.querySelectorAll("section");
+// let menuLi = document.querySelectorAll("header ul li a");
+// let section = document.querySelectorAll("section");
 
-function activeMenu() {
-  let len = section.length;
-  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
-  menuLi.forEach((sec) => sec.classList.remove("active"));
-  menuLi[len].classList.add("active");
-}
+// function activeMenu() {
+//   let len = section.length;
+//   while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+//   menuLi.forEach((sec) => sec.classList.remove("active"));
+//   menuLi[len].classList.add("active");
+// }
 
-activeMenu();
-window.addEventListener("scroll", activeMenu);
+// activeMenu();
+// window.addEventListener("scroll", activeMenu);
 
 // sticky navbar menu  ///////////////////////////////////////////////
 
-const header = document.querySelector("header");
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 50);
-});
+// const header = document.querySelector("header");
+// window.addEventListener("scroll", function () {
+//   header.classList.toggle("sticky", window.scrollY > 50);
+// });
 
 // toggle icon navbar  ///////////////////////////////////////////////
 
-let menuIcon = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".navlist");
+// let menuIcon = document.querySelector("#menu-icon");
+// let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
-};
+// menuIcon.onclick = () => {
+//   menuIcon.classList.toggle("bx-x");
+//   navlist.classList.toggle("open");
+// };
 
-window.onscroll = () => {
-  menuIcon.classList.remove("bx-x");
-  navlist.classList.remove("open");
-};
+// window.onscroll = () => {
+//   menuIcon.classList.remove("bx-x");
+//   navlist.classList.remove("open");
+// };
 
 // parallax /////////////////////////////////////////////////////
 
